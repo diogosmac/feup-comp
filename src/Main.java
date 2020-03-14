673123b4c8f0df40bbff57bce034fceb728ae7da
Marantesss <up201706917@fe.up.pt>
2020-03-14 18:28:08 +0000

    Worked on main method to read file as input stream

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
 public class Main {
-	
-	public static void main(String[] args) {
-		System.out.println("Write an arithmetic expression:");
 
+	public static void main(String[] args) {
+		System.out.println("Parser for programs in the Java-- language");
+
+		Parser parser;
+		// open file as input stream
+		try {
+			parser = new Parser(new java.io.FileInputStream(args[0]));
+		}
+		catch (java.io.FileNotFoundException e) {
+			System.out.println("ERROR: file " + args[0] + " not found.");
+			return;
+		}
+
+		// create Syntax Tree (returns reference to root node)
+		SimpleNode root = parser.Tree();
+
+		// Print tree to console
+		root.dump("");
 	}
 }
