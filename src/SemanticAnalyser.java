9e6a1532acc4bbdf651f71986caaf80b4eb19465
Marantesss <up201706917@fe.up.pt>
2020-04-23 19:03:47 +0100

    Started organizing semantic analysis per class, MAYBE IM CHANGING THIS

--- src/SemanticAnalyser.java
+++ src/SemanticAnalyser.java
@@ -16,7 +16,50 @@ public class SemanticAnalyser {
     }
 
     public void analyse() {
-        // get children
-        Node[] children = this.root.jjtGetChildren();
+        // start by root
+        this.analyseNode(this.root);
     }
+
+    private void analyseNode(SimpleNode node) {
+        // get all children
+        Node[] children = node.jjtGetChildren();
+        // visit all nodes
+        for (Node child : children) {
+            // Operators
+            if (child instanceof ASTand)
+                this.analyseAnd((ASTand) child);
+            else if (child instanceof ASTlt)
+                this.analyseLessThan((ASTlt) child);
+            else if (child instanceof ASTsum)
+                return;
+            else if (child instanceof ASTsub)
+                return;
+            else if (child instanceof ASTmult)
+                return;
+            else if (child instanceof ASTdiv)
+                return;
+            else if (child instanceof ASTnot)
+                return;
+            // control flow
+            else if (child instanceof ASTIfElseBlock)
+                return;
+            else if (child instanceof ASTWhileBlock)
+                return;
+            // declarations
+            else if (child instanceof ASTVarDeclaration)
+                return;
+            // repeat analysis for child nodes
+            this.analyseNode((SimpleNode) child);
+        }
+    }
+
+    private void analyseAnd(ASTand child) {
+
+    }
+
+    private void analyseLessThan(ASTlt child) {
+
+    }
+
+
 }
