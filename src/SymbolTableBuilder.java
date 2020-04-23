7fb0a96c6727e52f9770561aee3a134f364cfcb5
Marantesss <up201706917@fe.up.pt>
2020-04-23 11:50:16 +0100

    Added simple node to javacc files + started working on symbol table builder

--- src/SymbolTableBuilder.java
+++ src/SymbolTableBuilder.java
@@ -2,9 +2,41 @@ import SymbolTable.SymbolTable;
 
 public class SymbolTableBuilder {
 
-    public static SymbolTable buildSymbolTable(SimpleNode root) {
+    public SymbolTable buildSymbolTable(SimpleNode root) {
         SymbolTable table = new SymbolTable();
+        // get root's children
+        Node[] children = root.jjtGetChildren();
+        // find ASTClassDeclaration Node
+        for (Node child : children)
+            if (child instanceof ASTClassDeclaration)
+                this.processClassDeclaration((ASTClassDeclaration) child);
 
         return table;
+    }
+
+    private void processClassDeclaration(ASTClassDeclaration node) {
+        // get class declaration node children
+        Node[] children = node.jjtGetChildren();
+        // find variable and method declarations
+        for (Node child : children) {
+            if (child instanceof ASTVarDeclaration)
+                this.processVariableDeclaration((ASTVarDeclaration) child);
+            else if (child instanceof ASTRegularMethod)
+                this.processRegularMethodDeclaration((ASTRegularMethod) child);
+            else if (child instanceof ASTMainMethod)
+                this.processMainMethodDeclaration((ASTMainMethod) child);
+        }
+    }
+
+    private void processVariableDeclaration(ASTVarDeclaration node) {
+
+    }
+
+    private void processRegularMethodDeclaration(ASTRegularMethod node) {
+
+    }
+
+    private void processMainMethodDeclaration(ASTMainMethod node) {
+
     }
 }
