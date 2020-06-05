5eff29210412cf1a5a6b701e6d653e251818649c
Marantesss <up201706917@fe.up.pt>
2020-06-05 21:27:13 +0100

    Added documentation to parser ans symbol table

--- src/SymbolTable/VariableDescriptor.java
+++ src/SymbolTable/VariableDescriptor.java
@@ -1,7 +1,15 @@
 package SymbolTable;
 
+/**
+ * <h1>Variable Descriptor</h1>
+ * <p>A Variable Descriptor contains information regarding
+ * a variable or a method parameter.</p>
+ */
 public class VariableDescriptor extends Descriptor {
 
+    /**
+     * True if the variable
+     */
     private boolean initialised;
 
     public VariableDescriptor(String dataType) {
@@ -9,14 +17,29 @@ public class VariableDescriptor extends Descriptor {
         this.type = dataType;
     }
 
+    /**
+     * Return string with information regarding this
+     * variable descriptor.
+     *
+     * @param prefix indentation
+     * @return String with formatted information
+     */
     public String dump(String prefix) {
         return prefix + "Variable Descriptor: " + this.type;
     }
 
+    /**
+     * Getter for initialised attribute
+     * @return initialised
+     */
     public boolean isInitialised() {
         return initialised;
     }
 
+    /**
+     * Setter for initialised attribure
+     * @param initialised value to set
+     */
     public void setInitialised(boolean initialised) {
         this.initialised = initialised;
     }
